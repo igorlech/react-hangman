1b73d566be26d8ac69608beb111bd4e0ddc2b982
igorlech <eaaigle@students.eaaa.dk>
2023-03-23 21:17:44 +0100

    added JSON file and initial main markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
 function App() {
-  return <h1>Hello</h1>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+  return (
+    <div className="main">
+      <div className="end-text">Lose Win</div>
+    </div>
+    // <HangmanDrawing />
+    // <HangmanWord />
+    // <Keyboard />
+  );
 }
 
 export default App;
