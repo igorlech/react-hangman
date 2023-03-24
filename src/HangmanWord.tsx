cea17db65d5739fe24215ecd6c98538f418763c1
igorlech <eaaigle@students.eaaa.dk>
2023-03-24 20:59:28 +0100

    added initial HangmanWord logic

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,24 @@
 export function HangmanWord() {
-  return <div className="hangman-word"></div>;
+  const word = "hangman";
+  const guessedLetters = ["a", "n", "g"];
+  return (
+    <div className="hangman-word">
+      {word.split("").map((letter, index) => {
+        return (
+          <span className="letter-cont" key={index}>
+            <span
+              className="letter"
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? "visible"
+                  : "hidden",
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
 }
